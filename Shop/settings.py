99e4dbc9e6cc771a35601de26cba797acfaff8e2
Bhawana s <bhawana.shrestha@apexcollege.edu.np>
2023-05-09 22:56:05 +0545

    new app i.e Templates is created

--- Shop/settings.py
+++ Shop/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = os.path(BASE_DIR. 'Templates')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Products'
+    'Products',
+    'Accounts'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +59,7 @@ ROOT_URLCONF = 'Shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
